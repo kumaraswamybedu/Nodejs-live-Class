c7dd19fae685d791dd804cde420569afa61f520b
kumar B <kumaraswamy.b.edu@gmail.com>
2026-07-09 17:25:35 +0530

    #add new commands

--- Day-2/index.js
+++ Day-2/index.js
@@ -3,11 +3,17 @@
 //Global Objects
 global.console.log('hello');
 
+console.log('hello');
+
 //Process object
 //dotenv class
 
 console.log(__dirname); //print current folder name (path to the folder)
 console.log(__filename,'filename'); //print current file name (path to the file)  //print the cureent file path
   
+console.log(Buffer.from('hello'));//print the buffer of the string
+
+console.log(Buffer.from('hello').toString(),'stringified buffer'); //print the string from the buffer
+
 
 //18:00 sec 
